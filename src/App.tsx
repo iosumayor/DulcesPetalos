f5b212a2c48a619aa102a441490a41f2698cb1b2
iosumayor <iosumayor23@gmail.com>
2025-05-12 17:25:53 +0200

    feat: aplica color al background del layout y elimina estilos innecesarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./ui/views/Home/Home";
 import { ProductDetail } from "./ui/views/ProductDetail/ProductDetail";
