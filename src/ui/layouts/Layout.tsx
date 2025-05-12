f89d334b0b23eebfe5366f07c01ae09a99757614
iosumayor <iosumayor23@gmail.com>
2025-05-12 19:33:19 +0200

    feat: añade iconos de tema oscuro y claro, y actualiza el layout para soportar el cambio de tema

--- src/ui/layouts/Layout.tsx
+++ src/ui/layouts/Layout.tsx
@@ -4,11 +4,16 @@ import styles from "./Layout.module.scss";
 import { SearchBar } from "../views/Home/_components/SearchBar/SearchBar";
 import { useState } from "react";
 import { BackButton } from "../views/ProductDetail/_components/BackButton/BackButton";
+import { useThemeContext } from "../../core/contexts/ThemeContext/hooks/useThemeContext";
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const isHome = useLocation().pathname === "/";
+  const { theme } = useThemeContext();
   const [search, setSearch] = useState("");
+  console.log(theme);
   return (
-    <div className={styles.layoutContainer}>
+    <div
+      className={`${styles.layoutContainer} ${theme === "dark" ? styles.darkLayoutContainer : ""}`}
+    >
       <Header />
       {isHome ? (
         <SearchBar search={search} setSearch={setSearch} />
