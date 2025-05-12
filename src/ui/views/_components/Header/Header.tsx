36faee236a0683044e07ab5016ad8c8f1487f4cb
iosumayor <iosumayor23@gmail.com>
2025-05-12 16:59:10 +0200

    feat: añade el languageSelector al header

--- src/ui/views/_components/Header/Header.tsx
+++ src/ui/views/_components/Header/Header.tsx
@@ -8,14 +8,29 @@ export const Header = () => {
   const location = useLocation();
   const isProductDetail = location.pathname.includes("/item/");
   const { language, setLanguage, translate } = useLanguageContext();
+  const getButtonClassName = (buttonLanguage: string) => {
+    return `${styles.languageButton} ${language === buttonLanguage ? styles.active : ""}`;
+  };
+  console.log(language);
 
   return (
     <div className={styles.headerContainer}>
       <LinkTitle to="/">{translate("homeTitle")}</LinkTitle>
       <Breadcrumb isProductDetail={isProductDetail} />
-      <button onClick={() => setLanguage(language === "es" ? "en" : "es")}>
-        {language}
-      </button>
+      <div className={styles.languageButtonsContainer}>
+        <button
+          className={getButtonClassName("es")}
+          onClick={() => setLanguage("es")}
+        >
+          {translate("es")}
+        </button>
+        <button
+          className={getButtonClassName("en")}
+          onClick={() => setLanguage("en")}
+        >
+          {translate("en")}
+        </button>
+      </div>
     </div>
   );
 };
