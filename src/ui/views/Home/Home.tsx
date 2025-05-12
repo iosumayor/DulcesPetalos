ea10412884feb5d12636fdfffaa03563343e72ee
iosumayor <iosumayor23@gmail.com>
2025-05-12 12:54:35 +0200

    refactor: maneja en el Layout los componentes search y backButton dependiendo donde se encuentre

--- src/ui/views/Home/Home.tsx
+++ src/ui/views/Home/Home.tsx
@@ -1,15 +1,13 @@
 import styles from "./Home.module.scss";
-import { SearchBar } from "./_components/SearchBar/SearchBar";
 import { useHome } from "./hooks/useHome";
 import { Item } from "./_components/Item/Item";
 import type { Product } from "../../../core/domain/models/Products";
 import { Layout } from "../../layouts/Layout";
 
 export const Home = () => {
-  const { isLoading, filteredProducts, search, setSearch } = useHome();
+  const { isLoading, filteredProducts } = useHome();
   return (
     <Layout>
-      <SearchBar search={search} setSearch={setSearch} />
       {isLoading ? (
         <p>Cargando productos...</p>
       ) : (
--- src/ui/views/ProductDetail/ProductDetail.tsx
+++ src/ui/views/ProductDetail/ProductDetail.tsx
@@ -1,12 +1,10 @@
 import { useProductDetail } from "./hooks/useProductDetail";
 import { ProductItem } from "./_components/ProductItem/ProductItem";
-import { BackButton } from "./_components/BackButton/BackButton";
 import { Layout } from "../../layouts/Layout";
 export const ProductDetail = () => {
   const { product, isLoading } = useProductDetail();
   return (
     <Layout>
-      <BackButton />
       {isLoading ? (
         <p>Cargando producto...</p>
       ) : (
