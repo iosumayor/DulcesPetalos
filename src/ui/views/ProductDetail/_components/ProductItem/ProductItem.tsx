1e361e0579164a921557b2f8d9fc8f4b4124e398
iosumayor <iosumayor23@gmail.com>
2025-05-13 11:57:17 +0200

    feat: utiliza useMemo para no hacer renderizados innecesarios  de los products si no cambian el valor

--- src/ui/views/ProductDetail/_components/ProductItem/ProductItem.tsx
+++ src/ui/views/ProductDetail/_components/ProductItem/ProductItem.tsx
@@ -4,8 +4,35 @@ import {
 } from "../../../../../core/domain/models/Products";
 import styles from "./ProductItem.module.scss";
 import { useLanguageContext } from "../../../../../core/contexts/LanguageContext/Hooks/useLanguageContext";
+import { memo, useMemo } from "react";
+
 export const ProductItem = ({ product }: { product: Product | undefined }) => {
   const { translate } = useLanguageContext();
+
+  const productDetails = useMemo(() => {
+    if (!product) {
+      return {
+        name: "",
+        formattedPrice: "",
+        binomialName: "",
+        wateringsPerWeek: 0,
+        fertilizerType: "",
+        heightInCm: 0,
+      };
+    }
+    return {
+      name: product?.name,
+      price: getFormattedPrice(product ?? ({} as Product)),
+      binomialName: product?.binomialName,
+      wateringsPerWeek: product?.wateringsPerWeek,
+      fertilizerType:
+        product?.fertilizerType === "nitrogen"
+          ? translate("nitrogen")
+          : translate("phosphorus"),
+      heightInCm: product?.heightInCm,
+    };
+  }, [product, translate]);
+
   return (
     <div className={styles.productContainer}>
       <div className={styles.imageContainer}>
@@ -18,24 +45,23 @@ export const ProductItem = ({ product }: { product: Product | undefined }) => {
       <div className={styles.productInfo}>
         <h1>{product?.name}</h1>
         <p>
-          {translate("price")}: {getFormattedPrice(product ?? ({} as Product))}
+          {translate("price")}: {productDetails.price}
         </p>
         <p>
-          {translate("binomialName")}: {product?.binomialName}
+          {translate("binomialName")}: {productDetails?.binomialName}
         </p>
         <p>
-          {translate("wateringsPerWeek")}: {product?.wateringsPerWeek}
+          {translate("wateringsPerWeek")}: {productDetails?.wateringsPerWeek}
         </p>
         <p>
-          {translate("fertilizerType")}:{" "}
-          {product?.fertilizerType === "nitrogen"
-            ? translate("nitrogen")
-            : translate("phosphorus")}
+          {translate("fertilizerType")}:{productDetails?.fertilizerType}
         </p>
         <p>
-          {translate("height")}: {product?.heightInCm} cm
+          {translate("height")}: {productDetails?.heightInCm} cm
         </p>
       </div>
     </div>
   );
 };
+
+export const ProductItemComponent = memo(ProductItem);
