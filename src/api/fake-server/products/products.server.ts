4d0772833755c34b2ca0adcc9ab5b07eddafd674
iosumayor <iosumayor23@gmail.com>
2025-05-13 10:28:12 +0200

    feat: añade gestión de errores en el fake-server

--- src/api/fake-server/products/products.server.ts
+++ src/api/fake-server/products/products.server.ts
@@ -4,10 +4,17 @@ import productsData from "./products.get.res.example.json";
 
 export const getProducts = async (): Promise<ProductsDTO> => {
   await new Promise((resolve) => setTimeout(resolve, 500));
+  if (productsData.length === 0) {
+    throw new Error("No products found");
+  }
   return { products: productsData as Product[] };
 };
 
 export const getProductById = async (id: number): Promise<Product> => {
   await new Promise((resolve) => setTimeout(resolve, 500));
-  return productsData.find((product) => product.id === id) as Product;
+  const product = productsData.find((product) => product.id === id);
+  if (!product) {
+    throw new Error("Product not found");
+  }
+  return product as Product;
 };
